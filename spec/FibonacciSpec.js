6938c4d0c2deb5cbef85a5e82eb3b3dbde7271c2
CKKH <chriskkhassan@gmail.com>
2019-01-14 17:19:18 +0000

    Fibonacci can return second number in sequence (1)

--- spec/FibonacciSpec.js
+++ spec/FibonacciSpec.js
@@ -1,5 +1,9 @@
 describe ('Fibonacci', function() {
   it("returns 0 for the first number in the sequence", function(){
-    expect(Fibonacci(0)).toBe(0);
+    expect(Fibonacci(1)).toBe(0);
+  });
+
+  it("returns 1 if asked for the 2nd number of the sequence", function(){
+    expect(Fibonacci(2)).toBe(1);
   });
 });
--- src/Fibonacci.js
+++ src/Fibonacci.js
@@ -1,3 +1,7 @@
-function Fibonacci() {
-  return 0;
+function Fibonacci(num) {
+  if (num === 1) {
+    return 0
+  } else if (num === 2) {
+    return 1
+  }
 }
